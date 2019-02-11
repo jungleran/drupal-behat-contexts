4a9e55ab569ac20808792ce3dc87b823b85306b8
Chris Jansen <chris.jansen@ordina.nl>
2019-02-11 11:35:30 +0100

    Write html to artifacts dir on test failure

--- src/FailureContext.php
+++ src/FailureContext.php
@@ -32,4 +32,36 @@ class FailureContext implements Context {
     throw new \RuntimeException('Current url:' . $this->minkContext->getSession()->getCurrentUrl());
   }
 
+  /**
+   * @afterStep
+   *
+   * @param \Behat\Behat\Hook\Scope\AfterStepScope $scope
+   *
+   * @throws \RuntimeException
+   */
+  public function storeHtml(AfterStepScope $scope): void {
+    if ($scope->getTestResult()->getResultCode() !== TestResult::FAILED) {
+      return;
+    }
+
+    if (!$this->getSession()->isStarted()) {
+      return;
+    }
+
+    $dir = "/tmp/artifacts/html/";
+    if (!\is_dir($dir)) {
+      \mkdir($dir, 0777, TRUE);
+    }
+
+    $parts = [
+      \basename($scope->getFeature()->getFile()),
+      $scope->getStep()->getLine(),
+    ];
+
+    $filename = $dir . \implode('-', $parts) . '.html';
+    $outerHtml = $this->minkContext->getSession()->getPage()->getOuterHtml();
+    \file_put_contents($filename, $outerHtml);
+    throw new \RuntimeException('HTML WRITTEN TO: ' . $filename);
+  }
+
 }
